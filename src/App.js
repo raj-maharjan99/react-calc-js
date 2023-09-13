23ab4ad08c375b6056cf15728ce077d8dfdd9a8f
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-09-13 00:13:08 -0500

    give shape for calculator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import "./styles.css";
 
 function App() {
   return (
@@ -13,9 +13,11 @@ function App() {
       <button>1</button>
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
+      <button>+</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
